cc7b63135a81759d3377a092303086b0b04dee26
fredzgreen <fredzgreen@gmail.com>
2016-11-07 21:40:00 +0100

    With desktop client
    With desktop client

---
+++ LocalizationDatabase_Data.sql
@@ -0,0 +1,5 @@
+INSERT INTO "Languages" VALUES('sv_SE','Svenska',null,2);
+INSERT INTO "DefaultAudioLanguages" VALUES('sv_SE','English(US)');
+INSERT INTO "SteamLanguages" VALUES('swedish','sv_SE');
+INSERT INTO "LanguagePriorities" VALUES('sv_SE','sv_SE',100);
+INSERT INTO "FontStyleSheets" VALUES('sv_SE','Civ6_FontStyles_EFIGS.xml', 'MinionPro-Medium.otf');
